c9a2976d9aa50f3fd7ee3673ba0383275687711d
Simone-Corso <scorso26@gmail.com>
2023-11-15 16:34:15 +0100

    ho realizzato due variabili con un nome, e nella console presenta in modo corretto il gioco

--- js/index.js
+++ js/index.js
@@ -12,13 +12,13 @@
 //1.- creare una variabile per generare il numero casuale per il giocatore
 //  - andrò ad assegnare un numero randomico alla varibile"giocatore".
 //  - quindi userò la funzione del math.random, come dice l'esercizio dal 1 al 6.
-let x = Math.floor(Math.random() *6);
-
-console.log(x);
-
-//  - per far si che funzioni il numero a random, dovremmo creare un input di tipo "button" nell'HTML che genera un numero randomico.
+let giocatore = Math.floor(Math.random() *6 +1);
+console.log(giocatore);
 
 //2.- creare una variabile per generare il numero casuale per il computer
+//qua sotto inserisco la variabile per il computer
+let computer = Math.floor(Math.random() *6 +1);
+console.log(computer);
 //  - andrò ad assegnare un numero randomico alla varibile"computer".
 //  - quindi userò la funzione del math.random, come dice l'esercizio dal 1 al 6.
 //  - per far si che funzioni il numero a random, dovremmo creare un input di tipo "button" nell'HTML che genera un numero randomico.
