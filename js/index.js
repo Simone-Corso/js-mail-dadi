3dd97d45b01ac396db2cd727640dc850bf809212
Simone-Corso <scorso26@gmail.com>
2023-11-15 16:11:47 +0100

    un testo più dettagliato

--- js/index.js
+++ js/index.js
@@ -8,3 +8,13 @@
 //SE il computer lancia il dado, ed esce il 15
 //in questo caso il giocatore vince, perchè ha fatto uscire il numero più grande rispetto di quello che ha lanciato il computer.
 //Altrimenti : se usciva 15 per il giocatore e 24 per il computer ---> vince il computer.
+
+//1.- creare una variabile per generare il numero casuale per il giocatore
+//  - andrò ad assegnare un numero randomico alla varibile"giocatore".
+//  - quindi userò la funzione del math.random, come dice l'esercizio dal 1 al 6.
+//  - per far si che funzioni il numero a random, dovremmo creare un input di tipo "button" nell'HTML che genera un numero randomico.
+
+//2.- creare una variabile per generare il numero casuale per il computer
+//  - andrò ad assegnare un numero randomico alla varibile"computer".
+//  - quindi userò la funzione del math.random, come dice l'esercizio dal 1 al 6.
+//  - per far si che funzioni il numero a random, dovremmo creare un input di tipo "button" nell'HTML che genera un numero randomico.
