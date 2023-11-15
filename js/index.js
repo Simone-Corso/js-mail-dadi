d69296dc2717376a6b94edd4e1ac2a8971e542ba
Simone-Corso <scorso26@gmail.com>
2023-11-15 16:24:26 +0100

    verificare se funziona il numero randomico

--- js/index.js
+++ js/index.js
@@ -12,6 +12,10 @@
 //1.- creare una variabile per generare il numero casuale per il giocatore
 //  - andrò ad assegnare un numero randomico alla varibile"giocatore".
 //  - quindi userò la funzione del math.random, come dice l'esercizio dal 1 al 6.
+let x = Math.random();
+
+console.log(x);
+
 //  - per far si che funzioni il numero a random, dovremmo creare un input di tipo "button" nell'HTML che genera un numero randomico.
 
 //2.- creare una variabile per generare il numero casuale per il computer
