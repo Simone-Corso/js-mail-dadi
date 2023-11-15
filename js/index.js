a5f1f2c0bc65e4d52d419aaec48ec0839799a33d
Simone-Corso <scorso26@gmail.com>
2023-11-15 15:50:24 +0100

    scritto in italiano il problema da risolvere

--- js/index.js
+++ js/index.js
@@ -1,3 +1,10 @@
 // Gioco dei dadi
 //Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 //Stabilire il vincitore, in base a chi fa il punteggio più alto.
+
+// gioco dei dadi :
+// creare una logica per realizzare numeri a caso dal 1 al 6, sia per il giocatore e per il computer
+//SE il giocatore lancia il dado, ed esce il 24 
+//SE il computer lancia il dado, ed esce il 15
+//in questo caso il giocatore vince, perchè ha fatto uscire il numero più grande rispetto di quello che ha lanciato il computer.
+//Altrimenti : se usciva 15 per il giocatore e 24 per il computer ---> vince il computer.
